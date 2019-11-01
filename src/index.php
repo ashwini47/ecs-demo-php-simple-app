1ef4e73e4ebd07c8bdab012411f93f5fa566904f
Ashwini Singh <ashwini47@gmail.com>
2019-11-01 12:10:08 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple hello PHP App by Ashwini</h1>
-                <h2>Congratulations 1234567</h2>
+                <h2>Congratulations 22222222</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
